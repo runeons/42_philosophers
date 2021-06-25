e16b5a85cfbeb1467d542d1a647f74bdb2934dae
Tess SANTONI <tsantoni@student.42.fr>
2021-06-25 12:03:03 +0200

    monitor in main (less threads)

--- diner_routine.c
+++ diner_routine.c
@@ -1,26 +1,5 @@
 #include "philosophers.h"
 
-void	*monitor(void *phil)
-{
-	t_phil	*curr;
-
-	curr = ((t_phil *)phil);
-	while (1)
-	{
-		if (curr->eating_times == 0)
-			return (phil);
-		if (ret_current_time(*curr) - curr->last_eating > curr->time_to_die)
-		{
-			change_state_and_print(&curr, DIED);
-			pthread_mutex_unlock(curr->fork_left);
-			pthread_mutex_unlock(curr->fork_right);
-			return (phil);
-		}
-		if (*curr->end)
-			return (phil);
-	}
-	return (phil);
-}
 
 void	*routine(void *phil)
 {
@@ -56,6 +35,50 @@ void	*routine(void *phil)
 	return (phil);
 }
 
+void	*monitor(void *phil)
+{
+	t_phil	*curr;
+
+	curr = ((t_phil *)phil);
+	while (1)
+	{
+		if (curr->eating_times == 0)
+			return (phil);
+		if (ret_current_time(*curr) - curr->last_eating > curr->time_to_die)
+		{
+			change_state_and_print(&curr, DIED);
+			pthread_mutex_unlock(curr->fork_left);
+			pthread_mutex_unlock(curr->fork_right);
+			return (phil);
+		}
+		if (*curr->end)
+			return (phil);
+	}
+	return (phil);
+}
+
+int		monitor_main(t_phil *phil)
+{
+	t_phil	*curr;
+
+	curr = ((t_phil *)phil);
+	while (1)
+	{
+		if (curr->eating_times == 0)
+			return (1);
+		if (ret_current_time(*curr) - curr->last_eating > curr->time_to_die)
+		{
+			change_state_and_print(&curr, DIED);
+			pthread_mutex_unlock(curr->fork_left);
+			pthread_mutex_unlock(curr->fork_right);
+			return (1);
+		}
+		if (*curr->end)
+			return (1);
+	}
+	return (0);
+}
+
 int	start_diner(t_phil *phils, int nb_phil)
 {
 	void	*phil;
@@ -70,8 +93,14 @@ int	start_diner(t_phil *phils, int nb_phil)
 		phil = (void *) &phils[i];
 		if (pthread_create(&(phils[i].th_phil), NULL, &routine, phil) != 0)
 			return (print_error("Failed to create thread", NULL));
-		if (pthread_create(&(phils[i].th_monitor), NULL, &monitor, phil) != 0)
-			return (print_error("Failed to create thread", NULL));
+		// if (pthread_create(&(phils[i].th_monitor), NULL, &monitor, phil) != 0)
+		// 	return (print_error("Failed to create thread", NULL));
+	}
+	i = -1;
+	while (++i < nb_phil)
+	{
+		if (monitor_main(&phil[i]))
+			break;
 	}
 	i = -1;
 	while (++i < nb_phil)
@@ -79,8 +108,8 @@ int	start_diner(t_phil *phils, int nb_phil)
 		phil = (void **) &phils[i];
 		if (pthread_join(phils[i].th_phil, phil) != 0)
 			return (print_error("Failed to join thread", NULL));
-		if (pthread_join(phils[i].th_monitor, phil) != 0)
-			return (print_error("Failed to join thread", NULL));
+		// if (pthread_join(phils[i].th_monitor, phil) != 0)
+		// 	return (print_error("Failed to join thread", NULL));
 	}
 	return (0);
 }
